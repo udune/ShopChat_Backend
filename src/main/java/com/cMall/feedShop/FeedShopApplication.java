fe1f310b714dc1f8c138d900d0138ed6753a414a
JSM <jsm1592@gmail.com>
2025-06-27 12:58:06 +0900

    UserAuthServiceTest

--- src/main/java/com/cMall/feedShop/FeedShopApplication.java
+++ src/main/java/com/cMall/feedShop/FeedShopApplication.java
@@ -1,5 +1,6 @@
 package com.cMall.feedShop;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -11,6 +12,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class FeedShopApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // .env 파일을 로드합니다.
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue()) // .env의 키-값 쌍을 시스템 프로퍼티로 설정합니다.
+		);
 		SpringApplication.run(FeedShopApplication.class, args);
 	}
 }
