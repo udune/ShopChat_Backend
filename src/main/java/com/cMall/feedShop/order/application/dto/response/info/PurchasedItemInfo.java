2d6dc82dd5b1fe6641f96f4e47aff9535c093e28
udune <udune@naver.com>
2025-07-23 16:49:01 +0900

    fix: PurchasedItemInfo의 생성자 추가

--- src/main/java/com/cMall/feedShop/order/application/dto/response/info/PurchasedItemInfo.java
+++ src/main/java/com/cMall/feedShop/order/application/dto/response/info/PurchasedItemInfo.java
@@ -1,12 +1,16 @@
 package com.cMall.feedShop.order.application.dto.response.info;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PurchasedItemInfo {
     private Long orderItemId;
     private Long productId;
