4c0acc8fe3c36f8036d7978a2e45715acbdca696
udune <udune@naver.com>
2025-08-10 03:50:16 +0900

    불필요한 repository 삭제

--- src/main/java/com/cMall/feedShop/product/domain/repository/ProductImageRepository.java
+++ src/main/java/com/cMall/feedShop/product/domain/repository/ProductImageRepository.java
@@ -1,11 +1,10 @@
 package com.cMall.feedShop.product.domain.repository;
 
 import com.cMall.feedShop.product.domain.model.ProductImage;
-import com.cMall.feedShop.product.infrastructure.repository.ProductImageQueryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ProductImageRepository extends JpaRepository<ProductImage, Long>, ProductImageQueryRepository {
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
     Optional<ProductImage> findByImageId(Long imageId);
 }
