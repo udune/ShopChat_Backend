24e745b59372e121cd91ea06c073defee34e510d
udune <udune@naver.com>
2025-08-15 04:07:56 +0900

    feat: 가게 상세 조회

---
+++ src/main/java/com/cMall/feedShop/store/application/dto/response/StoreDetailResponse.java
@@ -0,0 +1,29 @@
+package com.cMall.feedShop.store.application.dto.response;
+
+import com.cMall.feedShop.store.domain.model.Store;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class StoreDetailResponse {
+    private Long storeId;
+    private String storeName;
+    private String description;
+    private String logo;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static StoreDetailResponse from(Store store) {
+        return StoreDetailResponse.builder()
+                .storeId(store.getStoreId())
+                .storeName(store.getStoreName())
+                .description(store.getDescription())
+                .logo(store.getLogo())
+                .createdAt(store.getCreatedAt())
+                .updatedAt(store.getUpdatedAt())
+                .build();
+    }
+}
