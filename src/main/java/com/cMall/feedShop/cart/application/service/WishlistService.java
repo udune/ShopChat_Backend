607c69ca0003832009c7ff404f0da7d41f79aa70
udune <udune@naver.com>
2025-08-16 04:40:48 +0900

    feat: 찜한 상품 삭제

---
+++ src/main/java/com/cMall/feedShop/cart/application/service/WishlistService.java
@@ -0,0 +1,49 @@
+package com.cMall.feedShop.cart.application.service;
+
+import com.cMall.feedShop.cart.domain.exception.CartException;
+import com.cMall.feedShop.cart.domain.model.WishList;
+import com.cMall.feedShop.cart.domain.repository.WishlistRepository;
+import com.cMall.feedShop.common.exception.ErrorCode;
+import com.cMall.feedShop.user.domain.model.User;
+import com.cMall.feedShop.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class WishlistService {
+
+    private final UserRepository userRepository;
+    private final WishlistRepository wishlistRepository;
+
+    public void deleteWishList(Long productId, String loginId) {
+        // 1. 사용자 조회
+        User currentUser = getCurrentUser(loginId);
+
+        // 2. 찜한 상품 조회
+        WishList wishlist = wishlistRepository.findByUserIdAndProductId(
+                currentUser.getId(), productId)
+                .orElseThrow(() -> new CartException(ErrorCode.WISHLIST_ITEM_NOT_FOUND));
+
+        // 3. 찜한 상품 삭제
+        wishlist.delete(LocalDateTime.now());
+
+        // 4. DB 저장
+        wishlistRepository.save(wishlist);
+    }
+
+    /**
+     * 현재 로그인한 사용자의 정보를 가져옵니다.
+     *
+     * @param loginId 현재 로그인한 사용자의 ID
+     * @return User 객체
+     */
+    private User getCurrentUser(String loginId) {
+        return userRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new CartException(ErrorCode.USER_NOT_FOUND));
+    }
+}
