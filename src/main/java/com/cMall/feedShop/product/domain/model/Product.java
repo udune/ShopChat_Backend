5aefc521cbf51d2b94f8568224d448c68bee1e3e
udune <udune@naver.com>
2025-08-16 19:31:11 +0900

    fix: decreaseWishNumber 제거 (현재 이슈에서는 불 필요)

--- src/main/java/com/cMall/feedShop/product/domain/model/Product.java
+++ src/main/java/com/cMall/feedShop/product/domain/model/Product.java
@@ -137,9 +137,4 @@ public class Product extends BaseTimeEntity {
         this.wishNumber = (this.wishNumber == null ? 0 : this.wishNumber) + 1;
     }
 
-    public void decreaseWishNumber() {
-        if (this.wishNumber != null && this.wishNumber > 0) {
-            this.wishNumber -= 1;
-        }
-    }
 }
