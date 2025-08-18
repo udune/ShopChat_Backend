0ed37e1e71821a2b5b6670f998925604e138ca42
udune <udune@naver.com>
2025-08-18 12:30:12 +0900

    fix: ProductReadServiceTest => ProductSellerReadServiceTest

--- src/test/java/com/cMall/feedShop/product/application/service/ProductSellerReadServiceTest.java
+++ src/test/java/com/cMall/feedShop/product/application/service/ProductSellerReadServiceTest.java
@@ -41,7 +41,7 @@ import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("ProductReadService 테스트")
-class ProductReadServiceTest {
+class ProductSellerReadServiceTest {
 
     @Mock
     private ProductRepository productRepository;
